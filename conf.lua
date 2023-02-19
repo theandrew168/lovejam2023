8242e3017ba2e0853eaa78aa40b818a74619fe9f
theandrew168 <andrew@shallowbrooksoftware.com>
2023-02-19 00:55:36 -0600

    Draw edge, add laser button, impl picking

--- conf.lua
+++ conf.lua
@@ -1,11 +1,15 @@
 function love.conf(t)
     local width = 10
     local height = 8
+
     local size = 64
     local border = 12
+    local box = size + border
 
-    local windowWidth = (size + border) * width + border
-    local windowHeight = (size + border) * height + border
+    local edge = 48
+
+    local windowWidth = (box * width) + border + (2 * edge)
+    local windowHeight = (box * height) + border + (2 * edge)
 
     t.title = "Khet"
     t.version = "11.4"
--- main.lua
+++ main.lua
@@ -18,6 +18,8 @@
 -- dynasty
 
 color = {
+    black = {0.0, 0.0, 0.0},
+    laser = {0.7, 0.2, 0.2},
     darkGray = {0.2, 0.2, 0.2},
     lightGray = {0.3, 0.3, 0.3},
     red = {0.6, 0.2, 0.2},
@@ -27,8 +29,15 @@ color = {
 size = {
     tile = 64,
     border = 12,
+    edge = 48,
 }
 size.box = size.tile + size.border
+
+button = {
+    xoff = size.edge + size.border + size.tile / 2,
+    yoff = size.edge / 2,
+    radius = (size.edge / 2) - 8,
+}
 
 board = {
     width = 10,
@@ -61,53 +70,128 @@ board = {
     },
 }
 
-
-function drawTile(x, y)
+function calcTilePoly(x, y)
     local poly = {
         -- top left
-        size.box * (x - 1) + size.border, size.box * (y - 1) + size.border,
+        size.box * (x - 1) + size.border + size.edge, size.box * (y - 1) + size.border + size.edge,
         -- top right
-        size.box * (x - 1) + size.box, size.box * (y - 1) + size.border,
+        size.box * (x - 1) + size.box + size.edge, size.box * (y - 1) + size.border + size.edge,
         -- bottom right
-        size.box * (x - 1) + size.box, size.box * (y - 1) + size.box,
+        size.box * (x - 1) + size.box + size.edge, size.box * (y - 1) + size.box + size.edge,
         -- bottom left
-        size.box * (x - 1) + size.border, size.box * (y - 1) + size.box,
+        size.box * (x - 1) + size.border + size.edge, size.box * (y - 1) + size.box + size.edge,
     }
-    love.graphics.polygon("fill", poly)
+    return poly
 end
 
-function love.update(dt)
+function dist(x1, y1, x2, y2)
+    return ((x2-x1)^2+(y2-y1)^2)^0.5
+end
 
+function pick(x, y)
+	local windowWidth, windowHeight = love.graphics.getDimensions()
+
+    local redX, redY = button.xoff, button.yoff
+    local redDist = dist(x, y, redX, redY)
+    if redDist <= button.radius then
+        return {"laser", "red"}
+    end
+
+    local silverX, silverY = windowWidth - button.xoff, windowHeight - button.yoff
+    local silverDist = dist(x, y, silverX, silverY)
+    if silverDist <= button.radius then
+        return {"laser", "silver"}
+    end
+
+    for ty = 1, board.height do
+        for tx = 1, board.width do
+            local poly = calcTilePoly(tx, ty)
+            local top, left = poly[2], poly[1]
+            local bottom, right = poly[6], poly[5]
+            if x >= left and x <= right and y >= top and y <= bottom then
+                return {"tile", tx, ty}
+            end
+        end
+    end
+
+    return nil
+end
+
+down = false
+function love.update(dt)
+    local x, y = love.mouse.getPosition()
+    if love.mouse.isDown(1) then
+        if not down then
+            local loc = pick(x, y)
+            if loc then
+                print(x, y, loc[1], loc[2], loc[3])
+            end
+        end
+        down = true
+    else
+        down = false
+    end
 end
 
 function love.draw(dt)
 	local windowWidth, windowHeight = love.graphics.getDimensions()
 
-    -- draw background / border
+    -- draw background
+    love.graphics.clear(color.black)
+
+    -- draw laser buttons
+    love.graphics.setColor(color.laser)
+    love.graphics.circle("fill",
+        -- top left
+        button.xoff,
+        button.yoff,
+        button.radius
+    )
+    love.graphics.circle("fill",
+        -- bottom right
+        windowWidth - button.xoff,
+        windowHeight - button.yoff,
+        button.radius
+    )
+
+    -- draw board background / border
     love.graphics.setColor(color.darkGray)
-    love.graphics.polygon("fill", 0, 0, windowWidth, 0, windowWidth, windowHeight, 0, windowWidth)
+    love.graphics.polygon("fill",
+        -- top left
+        size.edge, size.edge,
+        -- top right
+        windowWidth - size.edge, size.edge,
+        -- bottom right
+        windowWidth - size.edge, windowHeight - size.edge,
+        -- bottom left
+        size.edge, windowHeight - size.edge
+    )
 
     -- draw tiles
     love.graphics.setColor(color.lightGray)
     for y = 1, board.height do
         for x = 1, board.width do
-            drawTile(x, y)
+            local poly = calcTilePoly(x, y)
+            love.graphics.polygon("fill", poly)
         end
     end
 
     -- draw red tiles
     love.graphics.setColor(color.red)
     for _, p in ipairs(board.red) do
-        drawTile(p[1], p[2])
+        local poly = calcTilePoly(p[1], p[2])
+        love.graphics.polygon("fill", poly)
     end
 
     -- draw silver tiles
     love.graphics.setColor(color.silver)
     for _, p in ipairs(board.silver) do
-        drawTile(p[1], p[2])
+        local poly = calcTilePoly(p[1], p[2])
+        love.graphics.polygon("fill", poly)
     end
 
     -- draw pieces
+    -- TODO
     
     love.graphics.setColor(1,1,1)
 	local mx, my = love.mouse.getPosition()
